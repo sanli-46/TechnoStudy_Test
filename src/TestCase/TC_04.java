0ff90a71b5db0182b24a660fc99ee3531b3170f5
Snmtechno <sinem@henryyork.com>
2023-09-06 12:44:09 -0400

    Wait update

--- src/TestCase/TC_04.java
+++ src/TestCase/TC_04.java
@@ -18,47 +18,47 @@ public class TC_04 extends BaseDriver {
         JavascriptExecutor js=(JavascriptExecutor) driver;
         //Scroll...
         js.executeScript("window.scrollTo(0,7000);");
-        Func.Bekle(1);
+        Func.Wait(1);
 
         WebElement sDet=driver.findElement(By.linkText("SDET Yazılım Test Mühendisi"));
         sDet.click();
-        Func.Bekle(1);
+        Func.Wait(1);
 
         Assert.assertTrue(driver.getCurrentUrl().contains("sdet"));
-        Func.Bekle(1);
+        Func.Wait(1);
 
         driver.navigate().back();
-        Func.Bekle(1);
+        Func.Wait(1);
 
         WebElement androidApp=driver.findElement(By.linkText("Android Uygulama Geliştirme"));
         androidApp.click();
-        Func.Bekle(1);
+        Func.Wait(1);
 
         Assert.assertTrue(driver.getCurrentUrl().contains("androidapp"));
-        Func.Bekle(1);
+        Func.Wait(1);
 
         driver.navigate().back();
-        Func.Bekle(1);
+        Func.Wait(1);
 
         WebElement dataScience=driver.findElement(By.linkText("Veri bilimi"));
         dataScience.click();
-        Func.Bekle(1);
+        Func.Wait(1);
 
         Assert.assertTrue(driver.getCurrentUrl().contains("data"));
-        Func.Bekle(1);
+        Func.Wait(1);
 
         driver.navigate().back();
-        Func.Bekle(1);
+        Func.Wait(1);
 
         WebElement masterProgram=driver.findElement(By.linkText("Master's Program"));
         masterProgram.click();
-        Func.Bekle(1);
+        Func.Wait(1);
 
         Assert.assertTrue(driver.getCurrentUrl().contains("masters"));
-        Func.Bekle(1);
+        Func.Wait(1);
 
         driver.navigate().back();
-        Func.Bekle(1);
+        Func.Wait(1);
 
     }
 }
