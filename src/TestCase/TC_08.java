14a358a799ea5d26c9afe9f96d0bfb92c8d04876
Alperen Şanlı <alperensanli.46@gmail.com>
2023-09-06 20:08:28 +0300

    Alp

--- src/TestCase/TC_08.java
+++ src/TestCase/TC_08.java
@@ -1,2 +1,17 @@
-package TestCase;public class TC_08 {
+package TestCase;
+public class TC_08 {
+    @Test
+    public void Test2(){
+        driver.get("https://techno.study/tr");
+        try{
+            WebElement TermsOfUse = driver.findElement(By.xpath("//Span[text()='Şartları']"));
+            TermsOfUse.click();
+
+            WebElement page = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Kullanım Şartları']")));
+            Assert.assertTrue(page.isEnabled(), "Sayfa bulunamadı");
+        }catch (Exception e){
+            System.out.println("Elementi bulamadı");
+        }
+
+    }
 }
