87bbb81b85187ebfb9b7644daadd261ae561dd49
Snmtechno <sinem@henryyork.com>
2023-09-07 16:33:59 -0400

    update test6-1-

--- src/TestCase/AllTestCase.java
+++ src/TestCase/AllTestCase.java
@@ -4,6 +4,7 @@ import Utility.BaseDriver;
 import Utility.Func;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -127,29 +128,79 @@ public class AllTestCase extends BaseDriver {
     }
 
 
+
     @Test
+
     public void Test6() {
 
-        SoftAssert softAssert = new SoftAssert();
-
-        String strHomePage = "https://techno.study/tr";
-
         driver.get("https://techno.study/tr");
+        SoftAssert softAssert = new SoftAssert();
+        By[] clickableElements = {
+                By.linkText("SDET Yazılım Test Mühendisi"),
+                By.xpath("//div[text()='Android uygulama geliştirme']"),
+                By.linkText("Veri bilimi"),
+                By.xpath("//div[text()='Job Center & Arbeitsamt']"),
+                By.linkText("Master's Program"),
+                By.linkText("Kullanım Şartları"),
+                By.linkText("Gizlilik Politikası"),
+                By.linkText("Cookies Politikası"),
 
-        WebElement technoStudyLogo = driver.findElement(By.cssSelector("img[alt='TechnoStudy']"));
+        };
 
-        softAssert.assertTrue(technoStudyLogo.isDisplayed(), "Techno Study logosu görünmelidir.");
-        System.out.println("Techno Study logosu gorunuyor");
+        for (By elementLocator : clickableElements) {
+            try {
 
-        softAssert.assertTrue(technoStudyLogo.isEnabled(), "Techno Study logosu tıklanabilir olmalıdır.");
-        System.out.println("Techno Study logosu tiklanabilir durumda");
-        technoStudyLogo.click();
+                if( !elementLocator.equals(By.linkText("Kullanım Şartları")) &&
+                        !elementLocator.equals(By.linkText("Gizlilik Politikası")) &&
+                        !elementLocator.equals(By.linkText("Cookies Politikası"))) {
 
-        softAssert.assertTrue(driver.getCurrentUrl().equals("https://techno.study/tr"), "Ana sayfaya yönlendirilmelidir.");
+                    WebElement menuKurslar = driver.findElement(By.xpath("(//a[text()='Kurslar'])[1]"));
+                    Actions action = new Actions(driver);
+                    action.moveToElement(menuKurslar).perform();
+                    Func.Wait(1);
+                }
 
-        Assert.assertEquals("https://techno.study/home", strHomePage, "Oluşanla beklenen aynı değil");
+                if(elementLocator.equals(By.linkText("Kullanım Şartları")) ||
+                        elementLocator.equals(By.linkText("Gizlilik Politikası")) ||
+                        elementLocator.equals(By.linkText("Cookies Politikası")))
+                {
+
+                    WebElement elementToScroll = driver.findElement(elementLocator);
+                    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", elementToScroll);
+                    Func.Wait(2);
+                }
+
+                WebElement elementToClick = wait.until(ExpectedConditions.elementToBeClickable(elementLocator));
+                elementToClick.click();
+                Func.Wait(2);
+                checkAndClickLogo();
+
+                driver.navigate().to("https://techno.study/tr");
+            } catch (NoSuchElementException e) {
+                if (elementLocator.equals(By.xpath("//div[text()='Job Center & Arbeitsamt']"))) {
+                    driver.navigate().to("https://techno.study/tr");
+                } else {
+                    break;
+                }
+            }
+        }
 
         softAssert.assertAll();
+    }
+
+
+    private void checkAndClickLogo() {
+        SoftAssert softAssert = new SoftAssert();
+        By logoSelector = By.cssSelector("img[alt='TechnoStudy']");
+
+        WebElement logo = driver.findElement(logoSelector);
+
+        softAssert.assertTrue(logo.isDisplayed(), "TechnoStudy logosu görüntülenemiyor."); //Bu satır, logo adlı öğenin görüntülenip görüntülenmediğini kontrol ediyor
+
+        logo.click();
+
+        //Bu satır, şu anki URL'nin "https://techno.study/tr" olup olmadığını kontrol ediyor.
+        softAssert.assertEquals(driver.getCurrentUrl(), "https://techno.study/tr", "Logo'ya tıklanınca ana sayfaya yönlendirilmedi.");
 
     }
 
